1a1aa7c780f3b70d8b10dc3442280c032086fc12
James Madoc <136224820+jamfjc@users.noreply.github.com>
2026-04-15 19:37:15 +0200

    Add files via upload

---
+++ cpp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine()!.Split();
+            int n = int.Parse(line[0]);
+            int m = int.Parse(line[1]);
+
+            int[] timesTotal = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] lapTimes = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+                timesTotal[i] = lapTimes.Sum();
+            }
+            bool Improving = true;
+            for (int i =1; i<n;i++)
+            {
+                if (timesTotal[i] >= timesTotal[i - 1])
+                {
+                    Improving = false;
+                    break;
+                }
+
+            }
+            Console.WriteLine(Improving.ToString().ToLower());
+        }
+    }
+}
